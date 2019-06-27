46a42c9165c02c605c8e5ba20a60ca46f7f10aab
bfgoodman756 <38657302+bfgoodman756@users.noreply.github.com>
2019-06-27 17:00:13 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,19 +3,3 @@ $("figure").mouseleave(
     $(this).removeClass("hover");
   }
 );
-
-
-//menu
-$(".navToggle").click(function() {
-  $(this).toggleClass("active");
-  $(".overlay").toggleClass("open");
-  // this line ▼ prevents content scroll-behind
-});
-
-
-//menu close on menu item click
-$(".navToggleClose").click(function() {
-  $(".navBurger").removeClass("active");
-  $(".overlay").removeClass("open");
-  // this line ▼ prevents content scroll-behind
-});
