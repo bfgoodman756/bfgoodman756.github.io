4575f1657b78c7e4723cdd6c23ad78d038bb7138
bfgoodman756 <38657302+bfgoodman756@users.noreply.github.com>
2019-06-27 16:47:52 +0300

    Web

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ $("figure").mouseleave(
     $(this).removeClass("hover");
   }
 );
+
+
+//menu
+$(".navToggle").click(function() {
+  $(this).toggleClass("active");
+  $(".overlay").toggleClass("open");
+  // this line ▼ prevents content scroll-behind
+});
+
+
+//menu close on menu item click
+$(".navToggleClose").click(function() {
+  $(".navBurger").removeClass("active");
+  $(".overlay").removeClass("open");
+  // this line ▼ prevents content scroll-behind
+});
